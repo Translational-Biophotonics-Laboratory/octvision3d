d544c1c5fe5fc60bb4f142f21b01c11f96f168d1
Daniel Kermany <daniel.kermany@gmail.com>
2024-02-06 14:35:08 -0600

    reorganize into python package format

---
+++ octvision3d/utils.py
@@ -0,0 +1,14 @@
+from glob import glob
+import cv2
+import re
+
+
+def get_filenames(path, ext):
+    return sorted(glob(f"{path}/*.{ext}"))
+
+def bgr2rgb(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+def natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
+    return [int(text) if text.isdigit() else text.lower()
+            for text in _nsre.split(s)]
