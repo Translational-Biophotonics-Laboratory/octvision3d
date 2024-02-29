09b6552bb396f1106399d35a2df156b78d0b2401
Daniel Kermany <daniel.kermany@gmail.com>
2024-02-29 13:14:08 -0600

    Made a utils function natural_sort. Started check_unlabeled_pixels function

--- octvision3d/utils.py
+++ octvision3d/utils.py
@@ -9,6 +9,9 @@ def get_filenames(path, ext):
 def bgr2rgb(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-def natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
+def _natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
     return [int(text) if text.isdigit() else text.lower()
             for text in _nsre.split(s)]
+
+def natural_sort(l):
+    return sorted(l, key=lambda x: _natural_sort_key(x[0]))
