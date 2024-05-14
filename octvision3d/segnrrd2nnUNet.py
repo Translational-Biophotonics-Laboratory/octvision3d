0686ec8ef487ebad77287dc1be1c9e04a3863974
Daniel Kermany <daniel.kermany@gmail.com>
2024-05-14 17:59:15 -0500

    Added requirements.txt and finalized segnrrd2nnUNet.py

--- octvision3d/segnrrd2nnUNet.py
+++ octvision3d/segnrrd2nnUNet.py
@@ -3,11 +3,12 @@ import nrrd
 import argparse
 import numpy as np
 import tifffile as tif
+from tqdm import tqdm
 
-from octvision3d.utils import get_filenames,
-                              create_dataset_dirs,
-                              save_json,
-                              generate_dataset_json
+from octvision3d.utils import (get_filenames,
+                               create_dataset_dirs,
+                               save_json,
+                               generate_dataset_json)
 
 def segnrrd2nnUNet(path):
     """
@@ -17,15 +18,15 @@ def segnrrd2nnUNet(path):
         path (str): The directory path containing the input files.
     """
     
-    output_path = os.path.join(dir_path, "nnUNet_Dataset")
-
+    output_path = os.path.join(path, "nnUNet_Dataset")
+    
     # Create the necessary directories for the nnU-Net dataset
     create_dataset_dirs(output_path)
+    
     imagesTr = os.path.join(output_path, "imagesTr")
     labelsTr = os.path.join(output_path, "labelsTr")
-    
-    # Dictionary mapping segmentation labels to their corresponding
-    # numeric values
+
+    # Dictionary mapping segmentation labels to their corresponding numeric values
     labels_dict = {
         "CNV": 1,  # Choroidal Neovascularization
         "DRU": 2,  # Drusen
@@ -38,21 +39,21 @@ def segnrrd2nnUNet(path):
         "CHO": 9,  # Choroid
         "VIT": 10, # Vitreous
         "HYA": 11, # Hyaloid
-        "SHS": 12, # Subretinal Hyaloid Space 
+        "SHS": 12, # Sub-Hyaloid Space
         "ART": 13, # Artifacts
         "ERM": 14, # Epiretinal Membrane
         "SES": 15  # Sub-ERM Space
     }
 
-    # Retrieve paths for TIFF volumes and segmentation NRRD files, excluding those with
-    # "slo" in their names
-    vol_paths = [i for i in get_filenames(dir_path, ext="tif") if "slo" not in i]
-    seg_paths = [i for i in get_filenames(dir_path, ext="seg.nrrd") if "slo" not in i]
-
-    for vol_path, seg_path in zip(vol_paths, seg_paths):
+    # Retrieve paths for TIFF volumes and segmentation NRRD files, excluding those with "slo" in their names
+    vol_paths = [i for i in get_filenames(path, ext="tif") if "slo" not in i]
+    seg_paths = [i for i in get_filenames(path, ext="seg.nrrd") if "slo" not in i]
+    
+    for vol_path, seg_path in tqdm(zip(vol_paths, seg_paths), total=len(vol_paths)):
         # Ensure corresponding volume and segmentation files match by their basename
+        print(vol_path, seg_path)
         assert vol_path.split(".")[0] == seg_path.split(".")[0]
-
+        
         vol_name = os.path.splitext(os.path.basename(vol_path))[0]
         seg_name = os.path.basename(vol_path).split(".")[0]
 
@@ -60,19 +61,13 @@ def segnrrd2nnUNet(path):
         vol = tif.imread(vol_path)
         bitmap, header = nrrd.read(seg_path)
 
-        # Convert one-hot encoded bitmap to label array, flipping axes from (X, Y, Z)
-        # to (Z, Y, X)
+        # Convert one-hot encoded bitmap to label array, flipping axes from (X, Y, Z) to (Z, Y, X)
         labels = np.argmax(bitmap, axis=0).T
-
+        
         # Save spacing information as JSON
-        save_json(
-            {"spacing": [81.0, 1.0, 2.9]},
-            os.path.join(imagesTr, f"{vol_name}.json")
-        )
-        save_json(
-            {"spacing": [81.0, 1.0, 2.9]},
-            os.path.join(labelsTr, f"{seg_name}.json")
-        )
+        spacing = [81.0, 1.0, 2.9]
+        save_json({"spacing": spacing}, os.path.join(imagesTr, f"{vol_name}.json"))
+        save_json({"spacing": spacing}, os.path.join(labelsTr, f"{seg_name}.json"))
 
         # Save volume and label images as TIFF files
         output_tif = os.path.join(imagesTr, f"{vol_name}_0000.tif")
@@ -81,7 +76,7 @@ def segnrrd2nnUNet(path):
         tif.imwrite(output_labels, labels, photometric='minisblack')
 
     # Generate the dataset JSON file required by nnU-Net
-    generate_dataset_json(output_path,
+    generate_dataset_json(output_path, 
                           channel_names={"0": "OCT"},
                           labels=labels_dict,
                           file_ending=".tif",
